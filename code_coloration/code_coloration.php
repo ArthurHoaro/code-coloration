a38ae511690c643ac3e6d56eae6c979161b7e747
ArthurHoaro <arthur@hoa.ro>
2016-12-13 01:36:11 +0100

    Compatibility with Shaarli v0.8.0 - new config manager

--- code_coloration/code_coloration.php
+++ code_coloration/code_coloration.php
@@ -19,17 +19,17 @@ function hook_code_coloration_render_includes($data)
 /**
  * Adds PRISM JS file.
  *
- * @param array $data data passed to plugin
+ * @param array         $data data passed to plugin
+ * @param ConfigManager $conf instance.
  *
  * @return array altered $data.
  */
-function hook_code_coloration_render_footer($data)
+function hook_code_coloration_render_footer($data, $conf)
 {
     // Only execute when linklist is rendered.
     if ($data['_PAGE_'] == Router::$PAGE_LINKLIST) {
-        $filename = !empty($GLOBALS['plugins']['CUSTOM_PRISM_JS_FILENAME'])
-            ? $GLOBALS['plugins']['CUSTOM_PRISM_JS_FILENAME']
-            : 'prism.js';
+        $custom = $conf->get('plugins.CUSTOM_PRISM_JS_FILENAME');
+        $filename = !empty($custom) ? $custom : 'prism.js';
         $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/code_coloration/'. $filename;
     }
     return $data;
