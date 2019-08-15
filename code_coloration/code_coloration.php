5e6fa0be2ef447cd56a6e09cc7c7e8390367bc35
yomli <yomli@yom.li>
2019-08-15 07:54:01 +0200

    Adding custom css.

--- code_coloration/code_coloration.php
+++ code_coloration/code_coloration.php
@@ -8,14 +8,17 @@ use Shaarli\Config\ConfigManager;
  * Adds PRISM CSS file.
  *
  * @param array $data data passed to plugin
+ * @param ConfigManager $conf instance.
  *
  * @return array altered $data.
  */
-function hook_code_coloration_render_includes($data)
+function hook_code_coloration_render_includes($data, $conf)
 {
     // Only execute when linklist is rendered.
     if ($data['_PAGE_'] == Router::$PAGE_LINKLIST) {
-        $data['css_files'][] = PluginManager::$PLUGINS_PATH . '/code_coloration/prism.css';
+        $custom = $conf->get('plugins.CUSTOM_PRISM_CSS_FILENAME');
+        $filename = !empty($custom) ? $custom : 'prism.css';
+        $data['css_files'][] = PluginManager::$PLUGINS_PATH . '/code_coloration/' . $filename;
     }
     return $data;
 }
