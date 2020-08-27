822ebf5491e6e4af0af99640d2dd034cd0aa42d5
ArthurHoaro <arthur@hoa.ro>
2020-08-27 12:49:18 +0200

    Compatibility with Shaarli v0.12

--- code_coloration/code_coloration.php
+++ code_coloration/code_coloration.php
@@ -1,6 +1,6 @@
 <?php
 
-use Shaarli\Router;
+use Shaarli\Render\TemplatePage;
 use Shaarli\Plugin\PluginManager;
 use Shaarli\Config\ConfigManager;
 
@@ -15,7 +15,7 @@ use Shaarli\Config\ConfigManager;
 function hook_code_coloration_render_includes($data, $conf)
 {
     // Only execute when linklist is rendered.
-    if ($data['_PAGE_'] == Router::$PAGE_LINKLIST) {
+    if ($data['_PAGE_'] == TemplatePage::LINKLIST) {
         $custom = $conf->get('plugins.CUSTOM_PRISM_CSS_FILENAME');
         $filename = !empty($custom) ? $custom : 'prism.css';
         $data['css_files'][] = PluginManager::$PLUGINS_PATH . '/code_coloration/' . $filename;
@@ -34,7 +34,7 @@ function hook_code_coloration_render_includes($data, $conf)
 function hook_code_coloration_render_footer($data, $conf)
 {
     // Only execute when linklist is rendered.
-    if ($data['_PAGE_'] == Router::$PAGE_LINKLIST) {
+    if ($data['_PAGE_'] == TemplatePage::LINKLIST) {
         $custom = $conf->get('plugins.CUSTOM_PRISM_JS_FILENAME');
         $filename = !empty($custom) ? $custom : 'prism.js';
         $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/code_coloration/'. $filename;
